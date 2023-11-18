25732d9a1889edc3da0248b5336f5fc164252cab
povedica <yoyyosemite@gmail.com>
2023-11-18 15:36:14 +0100

    feat: AmmoPowerup now is a retro image and not a yello square.

--- js/PowerUps/AmmoPowerUp.js
+++ js/PowerUps/AmmoPowerUp.js
@@ -2,12 +2,18 @@ import PowerUp from '../PowerUp.js';
 
 export default class AmmoPowerUp extends PowerUp {
     constructor(ctx, x, y) {
-        super(ctx, x, y, 30, 30); // Tamaño del PowerUp de recarga
+        super(ctx, x, y, 'AmmoPowerUp'); // Tamaño del PowerUp de recarga
+        this.image = new Image();
+        this.image.src = 'images/ammo-powerup-100.png';
+        this.width = 100; // Ancho del power-up
+        this.height = 100; // Altura del power-up
     }
 
     draw() {
-        this.ctx.fillStyle = 'yellow'; // Color del PowerUp de recarga
-        this.ctx.fillRect(this.x, this.y, this.width, this.height);
+        // Asegúrate de que la imagen esté cargada antes de dibujarla
+        if (this.image.complete) {
+            this.ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+        }
     }
 
     activate(puchi) {
