891964423018922f43dc55f2dc97917ee56896b6
povedica <yoyyosemite@gmail.com>
2023-11-23 18:36:30 +0100

    feat: Hice banner cookie by default. Save in localstorage user cookie preferences

--- js/CookieLegal.js
+++ js/CookieLegal.js
@@ -1,9 +1,11 @@
+const cookiePolicyVersion = "1.0";
+
 function openModal() {
     var modal = document.getElementById("cookiePolicyModal");
     var contentContainer = document.getElementById("cookiePolicyContent");
 
     var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
+    xhr.onreadystatechange = function () {
         if (xhr.readyState == 4 && xhr.status == 200) {
             contentContainer.innerHTML = xhr.responseText;
             modal.style.display = "block";
@@ -20,9 +22,28 @@ function closeModal() {
 function acceptCookies() {
     // Aquí va el código para manejar la aceptación de cookies
     document.getElementById('cookieConsentPopup').style.display = 'none';
+    setCookieConsentInLocalStorage(true);
 }
 
 function rejectCookies() {
     // Aquí va el código para manejar el rechazo de cookies
-    document.getElementById('cookieConsentPopup').style.display = 'none';
+    setCookieConsentInLocalStorage(false)
 }
+
+function setCookieConsentInLocalStorage(consent) {
+    const consentData = {
+        consented: consent,
+        version: cookiePolicyVersion,
+        date: new Date().toISOString()
+    };
+    localStorage.setItem('cookieConsent', JSON.stringify(consentData));
+}
+
+window.onload = function () {
+    const consentString = localStorage.getItem('cookieConsent');
+
+    if (!consentString) {
+        document.getElementById('cookieConsentPopup').style.display = 'block';
+        return;
+    }
+};
